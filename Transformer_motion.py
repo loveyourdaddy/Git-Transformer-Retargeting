5285fb8d564c82141e21268fa651035b13b42d05
JIS <isjang08@kaist.ac.kr>
2021-08-18 16:50:43 +0900

    init commit

---
+++ Transformer_motion.py
@@ -0,0 +1,255 @@
+import json
+import torch
+from tqdm import tqdm
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import os
+from datasets import bvh_writer
+import option_parser
+from datasets import get_character_names, create_dataset
+from model import MotionGenerator
+# from torch.utils.tensorboard import SummaryWriter
+from datasets.bvh_parser import BVH_file
+from datasets.bvh_writer import BVH_writer
+import wandb
+
+""" motion data collate function """
+def motion_collate_fn(inputs): # input ???
+    # inputs : (32 batch, 2 character groups, 2 (motion, skeleton idx), 913 frames , 91 rotations and positoin of joints)
+    input_motions, output_motions = list(zip(*inputs)) #? output이 2개인 이유?
+
+    # TODO: padding 없애고 텐서로만 작업하기 
+    input_motions = torch.nn.utils.rnn.pad_sequence(input_motions, batch_first=True, padding_value=0)
+    output_motions = torch.nn.utils.rnn.pad_sequence(output_motions, batch_first=True, padding_value=0)
+    # dec_inputs = torch.nn.utils.rnn.pad_sequence(enc_inputs, batch_first=True, padding_value=0)
+
+    batch = [
+        input_motions,
+        output_motions
+        # dec_inputs
+    ]
+    return batch
+
+def train_epoch(args, epoch, model, criterion, optimizer, train_loader, train_dataset, bvh_writers, characters, save_name):
+    losses = [] # losses for 1 epoch 
+    loss_pos = []
+    model.train()
+
+    with tqdm(total=len(train_loader)-1, desc=f"TrainEpoch {epoch}") as pbar:
+        save_dir = args.save_dir + save_name
+        try_mkdir(save_dir)
+        for i, value in enumerate(train_loader):
+            optimizer.zero_grad()
+
+            # both (32,913,91)
+            input_motions, gt_motions = map(lambda v : v.to(args.cuda_device), value)
+
+            # Change Data Dimenstion 
+            # input_motions.transpose_(1,2)
+            # gt_motions.transpose_(1,2)
+
+            enc_inputs, dec_inputs = input_motions, input_motions
+            
+            # outputs: dec_outputs, enc_self_attn_probs, dec_self_attn_probs, dec_enc_attn_probs
+            output_motions = model(enc_inputs, dec_inputs) [0]
+
+            # Change Data Dimenstion 
+            # output_motions.transpose_(1,2)
+            # gt_motions.transpose_(1,2)
+            
+            """ Get Loss """
+            loss_sum = 0            
+            # for each motion in minibatch 
+            num_joints = gt_motions.size(1)
+            for j in range(args.batch_size):
+                # for all joints 
+                for k in range(num_joints):
+                    # loss for all frames of 1 joint
+                    loss = criterion(output_motions[j][k], gt_motions[j][k])
+                    loss_sum += loss
+                    losses.append(loss.item())
+                    if k == 0 :
+                        loss_pos.append(loss.item())
+  
+            """ Regularization Term """
+            # norm = torch.as_tensor(0.).cuda()
+            # for param in model.parameters():
+            #     norm += torch.norm(param)
+            # loss_sum += norm
+
+            loss_sum.backward()
+            optimizer.step()
+            pbar.update(1)
+            pbar.set_postfix_str(f"Rec_root_loss: {np.mean(loss_pos):.3f}, (mean: {np.mean(losses):.3f})") # 로그는 4개의 모션에 한번 꼴로 찍힙니다.
+
+            """ BVH Writing """
+            # Get Character Index
+            motion_idx = int(i * args.batch_size)
+            character_idx = int(motion_idx / args.num_motions)   
+
+            # denorm 
+            if args.normalization == 1:
+                gt_motions = train_dataset.denorm(1, character_idx, gt_motions)
+                output_motions = train_dataset.denorm(1, character_idx, output_motions)
+
+            # Write gt motion for 0 epoch
+            if epoch == 0:
+                save_dir_gt = save_dir + "character_{}/gt/".format(character_idx)
+                try_mkdir(save_dir_gt)
+                file = BVH_file(option_parser.get_std_bvh(dataset = characters[0][character_idx]))
+                bvh_writer = BVH_writer(file.edges, file.names)
+                for j in range(args.batch_size):
+                    file_name = save_dir_gt + "motion_{}.bvh".format(int(motion_idx % args.num_motions + j))
+                    motion = gt_motions[j]
+                    bvh_writer.write_raw(motion, 'quaternion', file_name)
+
+            # Write output motions for 10 epoch
+            if epoch % 50 == 0:
+                save_dir_output = save_dir + "character_{}/output_{}/".format(character_idx, epoch)
+                try_mkdir(save_dir_output)
+                file = BVH_file(option_parser.get_std_bvh(dataset = characters[0][character_idx]))
+                bvh_writer = BVH_writer(file.edges, file.names)
+                for j in range(args.batch_size):
+                    file_name = save_dir_output + "motion_{}.bvh".format(int(motion_idx % args.num_motions + j))
+                    motion = output_motions[j]
+                    bvh_writer.write_raw(motion, 'quaternion', file_name)
+
+            torch.cuda.empty_cache()
+            del gt_motions, enc_inputs, dec_inputs
+    
+    return np.mean(losses)
+
+def try_mkdir(path):
+    if not os.path.exists(path):
+        # print('make new dir')
+        os.system('mkdir -p {}'.format(path))
+
+""" eval """
+def eval_epoch(args, epoch, model, data_loader, data_dataset, vocab):
+    matchs = []
+    batch_size = data_loader.batch_size
+    model.eval()
+
+    save_dir = "./output/eval_result/"
+    try_mkdir(save_dir)
+
+    # validation set: included on training dataset, test set: not on training dataset
+    with tqdm(total=len(data_loader), desc=f"TestSet") as pbar: 
+        for i, value in enumerate(data_loader):
+            labels, enc_inputs, dec_inputs = map(lambda v : v.to(args.cuda_device), value)
+            outputs = model(enc_inputs, dec_inputs)
+
+            logits = outputs[0]
+
+            # max value, index
+            _, indices = logits.max(1)
+            
+            match = torch.eq(indices, labels).detach()
+            matchs.extend(match.cpu())
+            accuracy = np.sum(matchs) / len(matchs) if len(matchs) > 0 else 0
+
+            pbar.update(1)
+            pbar.set_postfix_str(f"Acc: {accuracy}:.3f")
+
+            if labels.shape[0] is not batch_size:
+                return accuracy
+
+            # save the result as text 
+            path = save_dir + str(i) +".txt"
+            f = open(path, 'w')
+            for j in range(0, batch_size):
+                f.write(str(j) + '\n')
+                f.write('\t input : ' + str(data_dataset.GetText(batch_size * i + j)) + '\n')
+                f.write('\t label : ' + str(labels[j]) + '\n')
+                f.write('\t logits: ' + str(logits[j]) + '\n')
+                f.write('\t logits: ' + vocab.id_to_piece(logits[j]) + '\n')  #check it is string
+            f.close()
+
+            #del
+            torch.cuda.empty_cache()
+            del labels, enc_inputs, dec_inputs
+
+    return np.sum(matchs) / len(matchs) if 0 < len(matchs) else 0
+
+def save(model, path, epoch):
+    try_mkdir(path)
+    path = os.path.join(path, str(epoch))
+    torch.save(model.state_dict(), path)
+    # print('Save at {} succeed!'.format(path))
+
+def load(model, path, epoch):
+    path = os.path.join(path, str(epoch))
+    print('loading from ', path)
+    if not os.path.exists(path):
+        raise Exception('Unknown loading path')
+    model.load_state_dict(torch.load(path))
+    print('load succeed')
+
+
+""" 0. Set Env Parameters """
+args = option_parser.get_args()
+args.cuda_device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+log_path = os.path.join(args.save_dir, 'logs/')
+wandb.init(project='transformer-retargeting', entity='loveyourdaddy')
+
+print("cuda availiable: {}".format(torch.cuda.is_available()))
+print("device: ", args.cuda_device)
+
+""" Changable Parameters """
+args.is_train = True # False 
+path = "./parameters/"
+save_name = "210817_transformer3__input_posEmb_conv1d/"
+
+
+""" 1. load Motion Dataset """
+characters = get_character_names(args)
+train_dataset = create_dataset(args, characters)
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=False, collate_fn=motion_collate_fn)
+print("characters:{}".format(characters))
+
+""" 2.Set Learning Parameters  """
+args.DoF = train_dataset.GetDoF()
+args.n_output = args.DoF # 91
+args.num_motions = len(train_dataset) / 4
+
+""" 3. Train and Test  """
+model = MotionGenerator(args, characters, train_dataset)
+model.to(args.cuda_device)
+wandb.watch(model)
+
+criterion = torch.nn.MSELoss() # torch.nn.CrossEntropyLoss() 
+optimizer = torch.optim.Adam(model.parameters(), lr = args.learning_rate)
+
+# Set BVH writers
+BVHWriters = []
+Files = []
+for i in range(len(characters)):
+    bvh_writers = [] 
+    files = []
+    for j in range(len(characters[0])):
+        file = BVH_file(option_parser.get_std_bvh(dataset=characters[i][j]))
+        files.append(file)
+        bvh_writers.append(BVH_writer(file.edges, file.names))
+
+    Files.append(files)
+    BVHWriters.append(bvh_writers)
+
+
+if args.is_train is True:
+    print("cuda device: ", args.cuda_device)
+    for epoch in range(args.n_epoch):
+        loss = train_epoch(args, epoch, model, criterion, optimizer, train_loader, train_dataset, BVHWriters, characters, save_name) # target_characters
+        wandb.log({"loss": loss})
+        save(model, path + save_name, epoch)
+
+else:
+    epoch = 49
+    load(model, path + save_name, epoch)
+    # 프레임별로 입력으로 주기. 
+    scores = []
+
+    # test score
+    # score = eval_epoch(args, epoch, model, test_loader, test_dataset, vocab)
+    # scores.append(score)
+
---
+++ model.py
@@ -0,0 +1,447 @@
+import json
+from pdb import set_trace
+import torch
+from torch import optim
+from tqdm import tqdm
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import os 
+import option_parser
+from datasets import get_character_names, create_dataset
+from models import create_model
+from models.base_model import BaseModel
+from model import *
+
+""" Attentnion Model """
+# higher level function from top to below
+# function of Q, K, V
+class ScaledDotProductAttention(nn.Module):
+    def __init__(self, d_head):        
+        super().__init__()
+        #d_head (64) : dim of key vector 
+        self.scale = 1 / (d_head ** 0.5)
+
+    def forward(self, Q, K, V, attn_mask):
+        # Q,K,V: (bs, n_head, window, DoF), attn_mask: (bs, n_head, window, window)
+
+        # (bs, n_head, window, window)
+        scores = torch.matmul(Q, K.transpose(-1, -2)).mul_(self.scale)
+        scores.masked_fill_(attn_mask, -1e9)
+
+        # Softmax on last dim 
+        attn_prob = nn.Softmax(dim = -1)(scores)
+        
+        context = torch.matmul(attn_prob, V)
+
+        # context:(bs, n_head, window, DoF) attn_prob (bs, n_head, window, window)
+        return context, attn_prob
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, args):
+        super().__init__()
+        # animation parameters
+        self.window_size = args.window_size
+        # self.window_size = args.window_size
+        # head parameters
+        self.d_head = args.d_head
+        self.n_head = args.n_head
+        # hidden vector dim
+        self.d_hidn = args.d_hidn
+        
+        """ Q, K, V Network """
+        # self.W_Q = nn.Conv1d(self.DoF, self.n_head * self.d_head, kernel_size=1) # W: (91, 256)
+        # self.W_K = nn.Conv1d(self.DoF, self.n_head * self.d_head, kernel_size=1)
+        # self.W_V = nn.Conv1d(self.DoF, self.n_head * self.d_head, kernel_size=1)
+        self.W_Q = nn.Linear(self.window_size, self.n_head * self.d_head) # W: (91, 256)
+        self.W_K = nn.Linear(self.window_size, self.n_head * self.d_head)
+        self.W_V = nn.Linear(self.window_size, self.n_head * self.d_head)
+
+        # Get attention value
+        self.scaled_dot_attn = ScaledDotProductAttention(self.d_head)
+        self.linear = nn.Linear(self.n_head * self.d_head, self.window_size)
+
+    def forward(self, Q, K, V, attn_mask):
+        # Q,K,V:(bs, window, DoF) attn_mask:(bs, window, window)
+
+        batch_size = Q.size(0)
+
+        """ Data Encoding 1 """
+        # (bs, window, n_head*d_head) -> (bs, n_head, window, d_head)
+        q_s = self.W_Q(Q).view(batch_size, -1, self.n_head, self.d_head).transpose(1, 2)
+        k_s = self.W_K(K).view(batch_size, -1, self.n_head, self.d_head).transpose(1, 2)
+        v_s = self.W_V(V).view(batch_size, -1, self.n_head, self.d_head).transpose(1, 2)
+
+        # head 갯수만큼 차원 추가 및 복사해두기
+        # (bs, window, window) -> (bs, n_head, window, window)
+        attn_mask = attn_mask.unsqueeze(1).repeat(1, self.n_head, 1, 1)
+        
+        # Attentinon 계산
+        # context:(bs, n_head, window, DoF) attn_prob (bs, n_head, window, window)
+        context, attn_prob = self.scaled_dot_attn(q_s, k_s, v_s, attn_mask)
+
+        # (bs, n_head, window, d_head) -> (bs, window, n_head * d_head)
+        context = context.transpose(1, 2).contiguous().view(batch_size, -1, self.n_head * self.d_head)        
+
+        # (bs, window, DoF)
+        output = self.linear(context)
+
+        return output, attn_prob, context
+
+""" Feed Forward """
+class PositionFeedForwardNet(nn.Module):
+    def __init__(self, args):
+        super().__init__()
+        # self.d_hidn = args.d_hidn
+        # self.window_size = args.window_size
+        self.window_size = args.window_size
+
+        # Layer
+        self.conv1 = nn.Conv1d(in_channels = self.window_size, out_channels = self.window_size * 4, kernel_size=1)
+        self.conv2 = nn.Conv1d(in_channels = self.window_size * 4, out_channels = self.window_size, kernel_size=1)
+        self.active = F.gelu
+    
+    def forward(self, inputs):
+        # (bs, window, DoF)
+
+        # window 차원에 conv 연산
+        output = self.active(self.conv1(inputs.transpose(1, 2)))
+        output = self.conv2(output).transpose(1, 2)
+
+        return output
+
+""" Layers"""
+class EncoderLayer(nn.Module):
+    def __init__(self, args):
+        super().__init__()
+        # animation parameters
+        self.args = args
+        self.window_size = args.window_size
+        self.layer_norm_epsilon = args.layer_norm_epsilon
+
+        # Layers
+        self.self_attn = MultiHeadAttention(self.args)
+        self.layer_norm1 = nn.LayerNorm(self.window_size, eps=self.layer_norm_epsilon)
+        self.pos_ffn = PositionFeedForwardNet(self.args)
+        self.layer_norm2 = nn.LayerNorm(self.window_size, eps=self.layer_norm_epsilon)
+
+    def forward(self, inputs, attn_mask):
+        # 아래 전부 (bs, window, DoF)
+
+        # attention 
+        att_outputs, attn_prob, context = self.self_attn(inputs, inputs, inputs, attn_mask)
+        # residual in encoder 
+        att_outputs = self.layer_norm1(inputs + att_outputs)
+
+        # ff
+        ffn_outputs = self.pos_ffn(att_outputs)
+        # residual  
+        ffn_outputs = self.layer_norm2(ffn_outputs + att_outputs)
+
+        return ffn_outputs, attn_prob, context
+
+""" feed forward """
+class PoswiseFeedForwardNet(nn.Module):
+    def __init__(self, args):
+        super().__init__()
+        self.d_hidn = args.d_hidn
+        self.window_size = args.window_size
+
+        # Layer
+        self.conv1 = nn.Conv1d(in_channels=self.window_size, out_channels=self.window_size * 4, kernel_size=1)
+        self.conv2 = nn.Conv1d(in_channels=self.window_size * 4, out_channels=self.window_size, kernel_size=1)
+        self.active = F.gelu
+
+    def forward(self, inputs):
+        # (bs, d_ff, n_seq)
+        tmp = self.conv1(inputs.transpose(1, 2))
+        output = self.active(tmp)
+
+        # (bs, n_seq, d_hidn)
+        output = self.conv2(output).transpose(1, 2)
+
+        # (bs, n_seq, d_hidn)
+        return output
+    
+class DecoderLayer(nn.Module):
+    def __init__(self, args):
+        super().__init__()
+        self.args = args
+        self.window_size = args.window_size
+
+        self.self_attn = MultiHeadAttention(self.args)
+        self.layer_norm1 = nn.LayerNorm(self.window_size, eps=self.args.layer_norm_epsilon)
+        self.dec_enc_attn = MultiHeadAttention(self.args)
+        self.layer_norm2 = nn.LayerNorm(self.window_size, eps=self.args.layer_norm_epsilon)
+        self.pos_ffn = PoswiseFeedForwardNet(self.args)
+        self.layer_norm3 = nn.LayerNorm(self.window_size, eps=self.args.layer_norm_epsilon)
+    
+    def forward(self, dec_inputs, enc_outputs, self_attn_mask, dec_enc_attn_mask):
+        
+        # dec_inputs에 Self attention을 적용합니다.
+        # (bs, DoF, d_hidn), (bs, n_head, DoF, DoF)        
+        self_att_outputs, self_attn_prob, _ = self.self_attn(dec_inputs, dec_inputs, dec_inputs, self_attn_mask) #Q, K, V, attn
+        self_att_outputs = self.layer_norm1(dec_inputs + self_att_outputs)
+        
+        # dec_enc attention을 적용합니다. 
+        # (bs, window, DoF), (bs, n_head, window, window)
+        dec_enc_att_outputs, dec_enc_attn_prob, _ = self.dec_enc_attn(self_att_outputs, enc_outputs, enc_outputs, dec_enc_attn_mask) 
+        dec_enc_att_outputs = self.layer_norm2(self_att_outputs + dec_enc_att_outputs)
+
+        # (bs, window, DoF)
+        ffn_outputs = self.pos_ffn(dec_enc_att_outputs)
+        # residual 
+        ffn_outputs = self.layer_norm3(dec_enc_att_outputs + ffn_outputs)
+
+        # (bs, window, DoF)
+        return ffn_outputs, self_attn_prob, dec_enc_attn_prob
+
+""" sinusoial encoding of each sentence """ 
+# n_seq: num of total seq(Sentence), d_hidn: 단어를 표시하는 벡터의 크기
+def get_sinusoid_encoding_table(n_seq, d_hidn):
+    # 포지션을 angle로 나타냄
+    def cal_angle(position, i_hidn):
+        return position / np.power(10000, 2 * (i_hidn // 2) / d_hidn)
+    def get_posi_ang_vec(position):
+        return [cal_angle(position, i_hidn) for i_hidn in range(d_hidn)]
+
+    sinusoid_table = np.array([get_posi_ang_vec(i_seq) for i_seq in range(n_seq)])
+    # 짝수: sin, 홀수: cos으로 sinusoial embedding 
+    # (bs, posiiton value)
+    sinusoid_table[:, 0::2] = np.sin(sinusoid_table[:, 0::2])
+    sinusoid_table[:, 1::2] = np.cos(sinusoid_table[:, 1::2])
+
+    # (DoF+1 , 32): each 프레임(int)들을 32dim의 float으로 나타냅니다
+    return sinusoid_table
+
+def get_attn_pad_mask(seq_q, seq_k, i_pad):
+    # seq: (bs, window, DoF)
+
+    # (32, 913, 91)
+    # batch_size, len_frames, len_pos = seq_q.size()
+    # batch_size, len_frames, len_pos = seq_k.size()
+    
+    # seq k에서 0인 부분을 <pad
+    valueTensor = torch.bmm(seq_q, seq_k.transpose(1,2))
+    pad_attn_mask = valueTensor.data.eq(i_pad)
+
+    # (bs,window,window)
+    return pad_attn_mask
+
+""" Encoder & Decoder """
+class Encoder(nn.Module):
+    def __init__(self, args):
+        super().__init__()
+        self.args = args
+        self.window_size = args.window_size
+        self.DoF = args.DoF
+
+        """  Input embedding: input 을 vector 로 나타내기 위한 """
+        # in: len of one input/output(num of embeddings), out: dim of hidden embedding dimension  
+        self.enc_input_emb = nn.Embedding(self.window_size, self.args.d_hidn)
+
+        """ Positional embedding: frame을 표시하기 위한 """
+        # frame을 나타내게 된다면, 과연 sinusoid로 표시할 필요가 있는가? check the paper 
+        sinusoid_table = torch.FloatTensor(get_sinusoid_encoding_table(self.args.DoF + 1, self.args.d_hidn))  # +1: 1 부터 시작하게끔
+        # frame을 int로 그냥 넣어주게 된다면 embedding이 아니다. 1
+        self.pos_emb = nn.Embedding.from_pretrained(sinusoid_table, freeze=True)
+
+        """ Layer """
+        self.layers = nn.ModuleList([EncoderLayer(self.args) for _ in range(self.args.n_layer)])
+        self.conv1 = nn.Conv1d(self.DoF, self.DoF, kernel_size=1) 
+
+    def forward(self, inputs):
+        # (bs, length of frames, joints): (4, 91, 64)
+
+        """ Positional encoding: input중 0에 해당하는 부분은 0으로 채우고, value가 있는 곳은 포지션 인덱스를 넣어줌. positions[0][0] : [0,1,2,3,...,64] """
+        # (DoF) -> (1,1,DoF)
+        frames_tensor = torch.arange(inputs.size(2), device=inputs.device, dtype=torch.int)
+        tmp1 = torch.unsqueeze(torch.unsqueeze(frames_tensor, 0), 0)        
+        # (1,1,DoF) -> (1,window,DoF)
+        one_motion_tensor = tmp1.expand(-1, inputs.size(1), -1)
+        # (1,window,DoF) -> (bs,window,DoF)
+        positions = one_motion_tensor.expand(inputs.size(0), -1, -1)
+        
+        # (bs, window, Dof)
+        pos_mask = inputs.eq(self.args.i_pad)
+        positions = positions.contiguous()
+        positions.masked_fill_(pos_mask, 0) 
+
+        """ 연산 """
+        # (bs, window, DoF)
+        # outputs = self.enc_input_emb(inputs.long()) + self.pos_emb(positions.long())        # 아마 float value가 인덱스로 들어가기 때문일겁니다.
+        
+        outputs = inputs + positions
+        outputs = self.conv1(outputs)
+        
+        # (bs, n_head, window, window)
+        attn_mask = get_attn_pad_mask(inputs, inputs, self.args.i_pad)
+        
+        # get all attn_prob of all layer
+        attn_probs = []
+        for layer in self.layers:
+            outputs, attn_prob, context = layer(outputs, attn_mask)
+            attn_probs.append(attn_prob)
+        
+        return outputs, attn_probs, context
+
+""" attention decoder mask: 현재단어와 이전단어는 볼 수 있고 다음단어는 볼 수 없도록 Masking 합니다. """
+def get_attn_decoder_mask(seq): 
+    
+    seq_tensor = torch.matmul(seq, seq.transpose(1,2))
+    subsequent_mask = seq_tensor.triu(diagonal=1) 
+    
+    return subsequent_mask
+
+class Decoder(nn.Module):
+    def __init__(self, args):
+        super().__init__()
+        self.args = args
+        self.window_size = args.window_size
+        self.DoF = args.DoF
+
+        self.dec_input_emb = nn.Embedding(self.window_size, self.args.d_hidn)
+        sinusoid_table = torch.FloatTensor(get_sinusoid_encoding_table(self.args.DoF + 1, self.args.d_hidn))
+        self.pos_emb = nn.Embedding.from_pretrained(sinusoid_table, freeze=True)
+
+        # layers
+        self.layers = nn.ModuleList([DecoderLayer(self.args) for _ in range(self.args.n_layer)])
+        self.conv1 = nn.Conv1d(self.DoF, self.DoF, kernel_size=1)
+
+    def forward(self, dec_inputs, enc_inputs, enc_outputs):
+
+        """  Positional Encoding : input중 0에 해당하는 부분은 0으로 채우고, value가 있는 곳은 포지션 인덱스를 넣어줍니다. """
+        # (64) -> (1,1,64)
+        frames_tensor = torch.arange(dec_inputs.size(2), device=dec_inputs.device, dtype=torch.int)
+        tmp1 = torch.unsqueeze(torch.unsqueeze(frames_tensor, 0), 0)        
+        # (1,1,64) -> (1,91,64)
+        one_motion_tensor = tmp1.expand(-1, dec_inputs.size(1), -1)
+        # (1,91,64) -> (4,91,64)
+        positions = one_motion_tensor.expand(dec_inputs.size(0), -1, -1)
+        
+        pos_mask = dec_inputs.eq(self.args.i_pad)
+
+        positions = positions.masked_fill(pos_mask, 0)
+
+        """ 연산 """
+        # (bs, DoF, d_hidn)
+        # dec_outputs = self.dec_input_emb(dec_inputs.long()) + self.pos_emb(positions.long())
+        dec_outputs = dec_inputs + positions
+        dec_outputs = self.conv1(dec_outputs)
+        
+        # (bs, DoF, DoF)
+        dec_attn_pad_mask = get_attn_pad_mask(dec_inputs, dec_inputs, self.args.i_pad)
+        # (bs, DoF, DoF)
+        dec_attn_decoder_mask = get_attn_decoder_mask(dec_inputs)
+
+        # (32, 913, 913)
+        dec_self_attn_mask = torch.gt((dec_attn_pad_mask + dec_attn_decoder_mask), 0)
+        # (bs, DoF, DoF)
+        dec_enc_attn_mask = get_attn_pad_mask(dec_inputs, enc_inputs, self.args.i_pad)
+
+        self_attn_probs, dec_enc_attn_probs = [], []
+        for layer in self.layers:
+
+            # (bs, DoF, d_hidn), (bs, DoF, DoF), (bs, DoF, DoF)
+            dec_outputs, self_attn_prob, dec_enc_attn_prob = layer(dec_outputs, enc_outputs, dec_self_attn_mask, dec_enc_attn_mask)
+
+            # 모든 layer의 attn 을 쌓기 
+            self_attn_probs.append(self_attn_prob)
+            dec_enc_attn_probs.append(dec_enc_attn_prob)
+
+        # (bs, DoF, d_hidn), [(bs, DoF, DoF)], [(bs, DoF, DoF)]
+        return dec_outputs, self_attn_probs, dec_enc_attn_probs
+
+""" Transoformer Model """    
+class Transformer(nn.Module):
+    def __init__(self, args):
+        super().__init__()
+        self.args = args
+        self.encoder = Encoder(args)
+        self.decoder = Decoder(args)
+    
+    def forward(self, enc_inputs, dec_inputs):
+        # input: (bs, window, DoF), output: (bs, window, DoF), ()
+        enc_outputs, enc_self_attn_probs, context = self.encoder(enc_inputs)
+        
+        # input: (bs, window, DoF), output: (bs, window, DoF), ()
+        dec_outputs, dec_self_attn_probs, dec_enc_attn_probs = self.decoder(dec_inputs, enc_inputs, enc_outputs)
+
+        return dec_outputs, enc_self_attn_probs, dec_self_attn_probs, dec_enc_attn_probs
+        
+
+class MotionGenerator(nn.Module):
+    def __init__(self, args, character_names, dataset):
+
+        """ Fully Connected Layer"""
+        # self.fc1 = nn.Linear(self.args.DoF, self.args.d_hidn)
+        # self.fc2 = nn.Linear(self.args.d_hidn, self.args.DoF)
+        """ 1d Conv layer """
+        # 91 -> 64 -> 91
+        # self.conv1 = nn.Conv1d(self.args.DoF, self.args.d_hidn, 1, padding=0) # d_hidn 개의 (output) kernel이 존재, kerner_size=3 # 15, 7
+        # self.active = F.gelu
+        # self.conv2 = nn.Conv1d(self.args.d_hidn, self.args.DoF, 1, padding=0)
+
+        """ 2d conv layer """
+        # self.conv1 = nn.Conv2d(self.args.DoF, self.args.d_hidn, (3,3), padding=1) # input_channel, output_channel, kernel(filter)_size
+        # self.active = F.gelu
+        # self.conv2 = nn.Conv2d(self.args.d_hidn, self.args.DoF, (3,3), padding=1)
+
+        """ 1d auto-encoder"""
+        # 91 -> 64 -> 91
+        # self.conv1 = nn.Conv1d(self.args.DoF, self.args.d_hidn, 3, padding=1) # d_hidn 개의 (output) kernel이 존재, kerner_size=3 # 15, 7
+        # self.active = F.gelu
+        # self.deconv1 = nn.ConvTranspose1d(self.args.d_hidn, self.args.DoF, 3, padding=1)
+
+        """ 2d auto-encoder"""
+        # self.conv1 = nn.Conv2d(1, 1, 3, padding=1) # d_hidn 개의 (output) kernel이 존재, kerner_size=3 # 15, 7
+        # self.active = F.gelu
+        # self.deconv1 = nn.ConvTranspose2d(1, 1, 3, padding=1)
+
+        """ Transformer """
+        # Parameters 
+        super().__init__()
+        self.args = args
+        self.window_size = args.window_size
+        # self.n_output = self.window_size
+
+        # layers
+        self.transformer = Transformer(args)
+        self.projection = nn.Linear(self.window_size, self.window_size)
+        # self.param = self.transformer.parameters() + self.projection.parameters()
+
+    """ Fuclly Connected layer Forward"""
+    # def forward(self, enc_inputs, dec_inputs):
+    #     x = self.fc1(enc_inputs)
+    #     x_act = nn.Softmax(dim = -1)(x)
+    #     y = self.fc2(x_act)
+    #     return y
+
+    """ Convolutional network Forward """
+    # def forward(self, enc_inputs, dec_inputs):
+    #     x = self.conv1(enc_inputs) 
+    #     x_pool = self.active(x)
+    #     y = self.conv2(x_pool)
+    #     # print(y.size())
+    #     # print(enc_inputs.size())
+    #     return y
+
+    """ auto-encoder Forward """
+    # def forward(self, enc_inputs, dec_inputs):
+    #     enc_inputs = torch.unsqueeze(enc_inputs,1)
+    #     x = self.conv1(enc_inputs)
+    #     x_act = self.active(x)
+    #     y = self.deconv1(x_act)
+    #     y = torch.squeeze(y,1)
+    #     return y
+        
+    """ Transofrmer """
+    def forward(self, enc_inputs, dec_inputs):
+        dec_outputs, enc_self_attn_probs, dec_self_attn_probs, dec_enc_attn_probs= self.transformer(enc_inputs, dec_inputs)
+        
+        output = self.projection(dec_outputs)
+
+        return output, enc_self_attn_probs, dec_self_attn_probs, dec_enc_attn_probs
+
+    # def parameters(self, recurse: bool) -> Iterator[Parameter]:
+    #     return super().parameters(recurse=recurse)
